6b63bb7414a98257cb12b634389d8e65f6e1d828
NullAspiration <173g1993@gmail.com>
2022-09-01 17:45:34 -0400

    PART 01 of LAB 02

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello");
+Console.WriteLine("Enter first number to be added: ");
+string Number_One =  Console.ReadLine() ?? "n/a";
+Console.WriteLine("Enter second number to be added: ");
+string Number_Two = Console.ReadLine() ?? "n/a";
+
+int Number_One_Convert = Convert.ToInt32(Number_One);
+int Number_Two_Convert = Convert.ToInt32(Number_Two);
+
+Console.WriteLine("SUM: " + (Number_One_Convert + Number_Two_Convert));
