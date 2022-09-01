fdb524e53650d12ea458dbe2f9f810762269ba20
NullAspiration <173g1993@gmail.com>
2022-09-01 18:10:37 -0400

    Installed Console tables to project

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello");
+﻿ 
+Console.WriteLine("Hello");
 Console.WriteLine("Enter first number to be added: ");
 string Number_One =  Console.ReadLine() ?? "n/a";
 Console.WriteLine("Enter second number to be added: ");
@@ -8,3 +9,5 @@ int Number_One_Convert = Convert.ToInt32(Number_One);
 int Number_Two_Convert = Convert.ToInt32(Number_Two);
 
 Console.WriteLine("The Sum of " + Number_One_Convert + " and " + Number_Two_Convert + " is" + "\nResult: " + (Number_One_Convert + Number_Two_Convert));
+
+Console.WriteLine("Multiply your numbers!");
