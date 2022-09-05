ec96075b4769a9b8dfbfa589b3b7dbfd7402bd70
69oo69 <53282201+69oo69@users.noreply.github.com>
2022-09-05 07:48:58 -0400

    updated master

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -3,18 +3,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //archaic UI
-        Console.WriteLine("Hello");
-        Console.WriteLine("Enter first number to be multiplied: ");
-        string Mult_Number_One = Console.ReadLine() ?? "n/a";
-        Console.WriteLine("Enter second number to be multiplied: ");
-        string Mult_Number_Two = Console.ReadLine() ?? "n/a";
-        //conversions to int32 from string
-        int Mult_Number_One_Convert = Convert.ToInt32(Mult_Number_One);
-        int Mult_Number_Two_Convert = Convert.ToInt32(Mult_Number_Two);
-        // multiply and dispaly output to user
-        Console.WriteLine("The product of " + Mult_Number_One_Convert + " and " + Mult_Number_Two_Convert + " is" + "\nResult: " + (Mult_Number_One_Convert * Mult_Number_Two_Convert));
-
 
     }
 }
