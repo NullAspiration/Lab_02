95912e37c9c64f811f51592c6b2224c628b0dee3
69oo69 <53282201+69oo69@users.noreply.github.com>
2022-09-05 07:27:54 -0400

    Created multiplication logic

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -3,18 +3,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //archaic UI
         Console.WriteLine("Hello");
-        Console.WriteLine("Enter first number to be added: ");
-        string Number_One = Console.ReadLine() ?? "n/a";
-        Console.WriteLine("Enter second number to be added: ");
-        string Number_Two = Console.ReadLine() ?? "n/a";
-
-        int Number_One_Convert = Convert.ToInt32(Number_One);
-        int Number_Two_Convert = Convert.ToInt32(Number_Two);
-
-        Console.WriteLine("The Sum of " + Number_One_Convert + " and " + Number_Two_Convert + " is" + "\nResult: " + (Number_One_Convert + Number_Two_Convert));
-
-        Console.WriteLine("Multiply your numbers!");
+        Console.WriteLine("Enter first number to be multiplied: ");
+        string Mult_Number_One = Console.ReadLine() ?? "n/a";
+        Console.WriteLine("Enter second number to be multiplied: ");
+        string Mult_Number_Two = Console.ReadLine() ?? "n/a";
+        //conversions to int32 from string
+        int Mult_Number_One_Convert = Convert.ToInt32(Mult_Number_One);
+        int Mult_Number_Two_Convert = Convert.ToInt32(Mult_Number_Two);
+        // multiply and dispaly output to user
+        Console.WriteLine("The product of " + Mult_Number_One_Convert + " and " + Mult_Number_Two_Convert + " is" + "\nResult: " + (Mult_Number_One_Convert * Mult_Number_Two_Convert));
 
 
     }
